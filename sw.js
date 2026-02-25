74f16c70eed66458f4a8218e2c0c74ed62416359
sikumel-ux <sikumel@gmail.com>
2026-02-25 23:14:53 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+ e.waitUntil(
+   caches.open('mahika-v1').then((cache) => cache.addAll([
+     'index.html',
+     'style.css',
+     'app.js'
+   ]))
+ );
+});
+
+self.addEventListener('fetch', (e) => {
+ e.respondWith(
+   caches.match(e.request).then((response) => response || fetch(e.request))
+ );
+});
